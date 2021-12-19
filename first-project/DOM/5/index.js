25bdc03ef9d3d07bb7c5ef8bc498c68be4bb29e4
i5nicca <info@5nicca.net>
2021-12-19 21:26:37 +0300

    DOM HW updated.

--- first-project/DOM/5/index.js
+++ first-project/DOM/5/index.js
@@ -45,6 +45,10 @@ let tasksList;
 // Функция создания новой задачи
 
 let newTask = () => {
+  // ð Н: Мне не очень симпатична такая реализация функции добавления новой задачи. Получается, что
+  // каждый раз при добавлении 1 задачи происходит перерисовка вообще всех записей на экране.
+  // Эта функция немножко слишком "много" на себя берет и занимается не только добавлением задачи к списку задач,
+  // но еще и рендерингом всего списка целиком.
   tasks.forEach((el) => {
     tasksList = document.querySelector(".tasks-list");
 
@@ -138,24 +142,47 @@ createTaskBlock.addEventListener("submit", (event) => {
       text: taskBlockInput.value,
     });
 
+    // ð Н: Лучше такие вещи объединять в какие-то смысловые модули, например:
+    // tasksListController = { add: func, render: func, cleanUp: func }
+    // и потом вызывать функцию tasksListController.cleanUp() где она нужна
+    // В больших кусках кода очень сложно будет отслеживать взаимосвязи между просто глобальными переменными типа tasksList
     tasksList.innerText = "";
 
+    // ð Н: Обман, получается, в названии. На деле не newTask, а renderTasksList или вроде того.
     newTask();
   }
 });
 
 // Удаление задачи из разметки и из массива
 
+// ð Н: Главная беда в том, что ты добавлял EventListener внутри другого EventListener
+//
+// Представь. Три задачи. Одно модальное окно. На каждой кнопке "Удалить" висит свой обработчик на click.
+// Ты нажимаешь на кнопку "Удалить" раз. Внутри модального окна "замыкаются" (ты уже знаком с замыканиями?)
+// со всеми id-шниками и target-ами два новых Listener-а.
+// Каждый последующий раз нажимая кнопку "Удалить" на других таргетах ты добавляешь еще пару Listener-ов
+// на те же события для тех же двух кнопок с новым набором данных. И при окончательном нажатии на "Удалить"
+// срабатывает уже не один обработчик события, а все, которые были созданы до последнего нажатия.
+//
+// Конструкция вложенных Listener-ов выглядит очень удобно на первый взгляд: можно просто пошарить target и
+// состояние между главным событием (Вызов модального окна) и подчиненным событием (Нажатие одной из двух кнопок)
+// Но вкладывать друг в друга listener-ы не стоит. Гипотетически, можно каждый раз при Вызове модального окна очищать
+// все Listener-ы с кнопок перед тем, как назначить новые, но эта идея тоже дерьмо.
+//
+// Как решить тогда вопрос передачи состояния между модальным окном и его подчиненными частями?
+// Можно завести глобальную переменную, которая бы хранила самый главный параметр открытого модального окна:
+let currentModalTargetId;
+
+// ð Н: мне нравится, что ты везде добавляешь проверки, это крайне полезно
 if (tasksList) {
   tasksList.addEventListener("click", (event) => {
     //
     let { target } = event;
-    console.log("target", target);
+    // console.log("target", target);
 
     const delButton = target.closest(".delete-button");
 
-    let delId = target.dataset.deleteTaskId;
-
+    currentModalTargetId = target.dataset.deleteTaskId;
     let delAction = false;
 
     //
@@ -171,51 +198,7 @@ if (tasksList) {
 
     // let yourChoice = (id) => {
     //
-    if (cancelButton) {
-      cancelButton.addEventListener("click", (event) => {
-        // console.log("Нажата кнопка Отмена / cancelButton", cancelButton);
 
-        modalOverlay.classList.add("modal-overlay_hidden");
-        return;
-      });
-    }
-
-    //
-
-    if (confirmButton) {
-      confirmButton.addEventListener("click", (event) => {
-        // console.log("Нажата кнопка Удалить / confirmButton", confirmButton);
-
-        const targetItem = target.closest(".task-item");
-
-        console.log("targetItem", targetItem);
-
-        // delAction = true;
-
-        targetItem.remove();
-
-        let delId = target.dataset.deleteTaskId - 1;
-
-        console.log("delId", delId);
-
-        delete tasks[delId];
-
-        //
-
-        // tasks = tasks.filter((n) => n);
-
-        // tasks.forEach((element) => {
-        //   if (!element) {
-        //     console.log(index);
-        //     task.splice(index, 1);
-        //   }
-        // });
-
-        //
-
-        modalOverlay.classList.add("modal-overlay_hidden");
-      });
-    }
     //
     // };
 
@@ -227,6 +210,59 @@ if (tasksList) {
     //
 
     // console.log("target.dataset.deleteTaskId", target.dataset.deleteTaskId);
+  });
+}
+
+// ð Н: Я вынесла эти два обработчика на уровень выше
+if (cancelButton) {
+  cancelButton.addEventListener("click", () => {
+    // console.log("Нажата кнопка Отмена / cancelButton", cancelButton);
+
+    currentModalTargetId = null;
+    modalOverlay.classList.add("modal-overlay_hidden");
+    return;
+  });
+}
+
+//
+
+if (confirmButton) {
+  confirmButton.addEventListener("click", () => {
+    // console.log("Нажата кнопка Удалить / confirmButton", confirmButton);
+
+    const targetItem = document.querySelector(`.task-item[data-task-id="${currentModalTargetId}"]`);
+    console.log("targetItem", targetItem);
+
+    // delAction = true;
+
+    targetItem.remove();
+
+    let delId = currentModalTargetId - 1;
+
+    console.log("delId", delId);
+
+    // ð Н: Тут можно сплайсить (тогда не придется искать способ почистить empty поля),
+    // НО! нужно будет после удаления одного элемента перерисовать список целиком, потому что id у элементов массива
+    // поменяются (пересчитаются, особенно если из середины удалим).
+    // tasks.splice(delId, 1);
+
+    // ð Н: Когда случается delete наш дорогой массив конвертируется в объект
+    // В данном случае это даже удобно получается, просто FYI, что delete это
+    // только для объектов
+    delete tasks[delId];
+
+    // tasks = tasks.filter((n) => n);
+
+    // tasks.forEach((element) => {
+    //   if (!element) {
+    //     console.log(index);
+    //     task.splice(index, 1);
+    //   }
+    // });
+
+    //
+
+    modalOverlay.classList.add("modal-overlay_hidden");
   });
 }
 
