35d1272c9e2258982c8a1add9ae0635d7932c112
DEM84 <dem84@yandex.ru>
2022-01-22 22:21:40 +0300

    Вебпак 4

--- first-project/WebpackRollup/3/index.js
+++ first-project/WebpackRollup/3/index.js
@@ -1,1 +1,13 @@
-console.log("hello, world");
+import "./index.css";
+import JSIMG from "./assets/js.png";
+
+console.log("Hello World 3!!!");
+
+const header = document.createElement("h1");
+const image = document.createElement("img");
+
+header.innerText = "I love JavaScript";
+image.src = JSIMG;
+
+document.body.append(header);
+document.body.append(image);
--- first-project/WebpackRollup/3/webpack.config.js
+++ first-project/WebpackRollup/3/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -6,5 +7,42 @@ module.exports = {
   output: {
     filename: "build.js",
     path: path.resolve(__dirname, "build"),
+  },
+  devServer: {
+    open: true,
+    compress: true,
+    port: 4000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "Development 3",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
